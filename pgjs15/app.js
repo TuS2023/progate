619ec98aca0c2bc4efc4d1a7e38696c5853fde3b
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-17 08:20:28 +0000

    else条件分岐

---
+++ pgjs15/app.js
@@ -0,0 +1,23 @@
+const number = 7;
+if (number > 10){
+  console.log("numberは10より大きいです");
+}
+if (number <= 10){
+  console.log("numberは10以下です");
+}
+
+
+if (number > 10){
+  console.log("numberは10より大きいです");
+} else {
+  console.log("numberは10以下です");
+}
+
+const age = 17;
+
+// 条件式が成り立たない場合に「私は20歳未満です」と出力してください
+if (age >= 20) {
+  console.log("私は20歳以上です");
+} else {
+  console.log("私は20歳未満です");
+}
