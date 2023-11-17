c25b3013e5db9c5c5a40f7456cb2d9a4b6397cc9
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-17 08:12:47 +0000

    真偽値と比較演算子2

---
+++ pgjs14/app.js
@@ -0,0 +1,21 @@
+const number = 12;
+console.log(number == 12);
+console.log(number == "12");
+console.log(number === 12);
+console.log(number === "12");
+const name = "john";
+console.log(name != "john");
+
+const password = "ninjawanko";
+
+// passwordの値が"ninjawanko"の場合、「ログインに成功しました」と出力してください
+if(password === "ninjawanko"){
+  console.log('ログインに成功しました');
+}
+
+
+
+// passwordの値が"ninjawanko"でない場合、「パスワードが間違っています」と出力してください
+if(password !== "ninjawanko"){
+  console.log('パスワードが間違っています');
+}
