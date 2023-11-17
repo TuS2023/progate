0fa0822ed0a0607496caa08e0d9a0bca61020941
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-17 09:05:40 +0000

    switch1

---
+++ pgjs18/app.js
@@ -0,0 +1,31 @@
+const color = "赤";
+switch(color){
+  case "赤":
+    console.log("ストップ！");
+    
+  case "黄色":
+    console.log("要注意");
+    break;
+}
+
+const n = 2;
+
+switch (n) {
+  case 1:
+    console.log("大吉です");
+    break;
+
+  // nの値が2のcaseを追加してください
+  case 2:
+    console.log("吉です");
+    break;
+  
+  
+  
+  // nの値が3のcaseを追加してください
+  case 3:
+    console.log("小吉です");
+    break;
+  
+  
+}
