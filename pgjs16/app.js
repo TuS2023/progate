0c65bad69558690797e03fa0c8aabdd75ff334fa
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-17 08:54:11 +0000

    else if

---
+++ pgjs16/app.js
@@ -0,0 +1,19 @@
+const number = 7;
+if (number > 10){
+  console.log("numberは10より大きいです");
+} else if (number > 5){
+  console.log("numberは5より大きいです");
+} else {
+  console.log("numberは5以下です");
+}
+
+const age = 17;
+
+// ageの値が10以上20未満のとき、「私は20歳未満ですが、10歳以上です」と出力してください
+if (age >= 20) {
+  console.log("私は20歳以上です");
+} else if (age >= 10){
+  console.log("私は20歳未満ですが、10歳以上です");
+ } else {
+  console.log("私は10歳未満です");
+}
