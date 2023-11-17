d1cd90364929803762968711a1c7744356106d24
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-11-17 07:37:15 +0000

    真偽値、比較演算子

---
+++ pgjs13/app.js
@@ -0,0 +1,14 @@
+const number = 12;
+console.log(number > 10);
+if (number > 10){
+  console.log("numberは10より大きいです");
+}
+const age = 24;
+// 「age >= 20」を出力してください
+console.log(age >= 20);
+// 「age < 20」を出力してください
+console.log(age < 20);
+// ageの値が20以上の場合に、「私は20歳以上です」と出力してください
+if (age >= 20){
+  console.log("私は20歳以上です");
+}
