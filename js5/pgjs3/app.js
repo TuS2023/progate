8d14306c6f5f796c1d5788549746d87ac5f02e30
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-12-02 14:43:07 +0000

    コンストラクタ(1)

---
+++ js5/pgjs3/app.js
@@ -0,0 +1,17 @@
+class Animal {
+  constructor() {
+    console.log("こんにちは！")
+  }
+}
+const animal1 = new Animal();
+const animal2 = new Animal();
+
+class Animal {
+  // コンストラクタを追加してください
+  constructor() {
+    console.log("インスタンスを生成しました")
+  }
+  
+}
+
+const animal = new Animal();
