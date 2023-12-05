0f2c489adfb0c8350e12ed8f21826d3c52485dda
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2023-12-05 02:53:21 +0000

    パッケージ1

---
+++ js6/pgjs6/data/dogData.js
@@ -0,0 +1,9 @@
+// Dogクラスのインポートと定数dogを以下に張り付けてください
+import Dog from "../class/dog";
+
+const dog1 = new Dog("レオ", 4, "チワワ");
+const dog2 = new Dog("ベン", 2, "プードル");
+
+// 定数dogをエクスポートしてください
+
+export { dog1, dog2 }
