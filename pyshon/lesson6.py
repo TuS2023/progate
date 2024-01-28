da06988eacb4b51bc898a016a57a446e67d0c8e2
EC2 Default User <ec2-user@ip-172-31-19-152.ap-northeast-1.compute.internal>
2024-01-28 09:04:56 +0000

    リストの要素の更新・追加

---
+++ pyshon/lesson6.py
@@ -0,0 +1,17 @@
+fruits = ['apple', 'banana', 'orange']
+
+print(fruits[0])
+
+print('好きな果物は' + fruits[2] + 'です')
+
+
+fruits = ['apple', 'banana', 'orange']
+
+fruits.append('grape')
+
+print(fruits)
+
+fruits[0] = 'cherry'
+
+# インデックス番号が 0 の要素を出力してください
+print(fruits[0])
